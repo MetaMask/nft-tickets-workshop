c8e19ccc6f81427fa22f19d89d83cd4ca227c5b0
Eric Bishard <e.bishard@gmail.com>
2022-02-28 03:08:01 -0800

    Add OpenZeppelin contract for our `Foxcon2022` NFT Ticket project Update Solidity version everywhere to `^0.8.1`

--- contracts/Foxcon2022.sol
+++ contracts/Foxcon2022.sol
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.4;
 
-contract Foxcon2022 {
-  constructor() public {
-  }
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract Foxcon2022 is ERC721, ERC721URIStorage {
+    constructor() ERC721("Foxcon2022", "FXC22") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs";
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
