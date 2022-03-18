9129f8006002c030e880251c0cbe4ec7ee288640
Alaa Hadad <alaahd@gmail.com>
2022-03-18 14:33:53 +0400

    adding loader for minting tickets

--- src/components/templates/TicketDetails.js
+++ src/components/templates/TicketDetails.js
@@ -1,10 +1,13 @@
-import { useContext } from 'react'
-import styled from 'styled-components'
-import { ViewContext } from '../../context/ViewProvider'
+import { useCallback } from "react";
+import styled from "styled-components";
+import { useViewContext } from "../../context/ViewProvider";
+import { MutatingDots } from "react-loader-spinner";
 
 const TicketDetails = ({ ticket }) => {
-  const { user, foxcon2022, chainId } = useContext(ViewContext)
-  const { address } = user
+  const { user, foxcon2022, chainId, isMinting, setIsMinting } =
+    useViewContext();
+
+  const { address } = user;
 
   const NftCard = styled.div`
     width: 300px;
@@ -12,11 +15,11 @@ const TicketDetails = ({ ticket }) => {
     border-radius: 12px;
     border: 1px solid #cfcfcf;
     margin: 8px;
-  `
+  `;
 
   const NftCollName = styled.div`
     padding: 8px;
-  `
+  `;
 
   const InnerCont = styled.div`
     display: flex;
@@ -24,20 +27,40 @@ const TicketDetails = ({ ticket }) => {
     padding: 8px;
     color: #222;
     button {
-      background-color: #FFF;
+      background-color: #fff;
       color: inherit;
     }
-  `
+  `;
 
   const NftName = styled.div`
     font-weight: 600;
-  `
+  `;
 
-  const mintTicket = async () => {
-    console.log("minting start")
-      foxcon2022.mintItem({
+  const StyledLoader = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.5);
+  `;
+
+  const FullScreenLoader = () => (
+    <StyledLoader>
+      <MutatingDots color="orange" ariaLabel="loading-indicator" />
+    </StyledLoader>
+  );
+
+  const mintTicket = useCallback(() => {
+    console.log("minting start");
+    setIsMinting(true);
+    foxcon2022
+      .mintItem({
         from: address,
-        value: ticket.priceHexValue
+        value: ticket.priceHexValue,
       })
       // signer.sendTransaction({
       //   from: address,
@@ -46,29 +69,41 @@ const TicketDetails = ({ ticket }) => {
       //   data: ticket.data,
       //   chainId: ticket.chainId
       // })
-      .then((tx) => console.log(`Minting complete, mined: ${tx}`))
-      .catch((error) => console.error(error))
-  }
+      .then((tx) => {
+        console.log(`Minting complete, mined: ${tx}`);
+      })
+      .catch((error) => {
+        console.error(error);
+        setIsMinting(false);
+      });
+  }, [address, foxcon2022, setIsMinting, ticket.priceHexValue]);
 
   return (
     <>
+      {isMinting && <FullScreenLoader />}
       <NftCard>
-        <img width="300" height="300" src={ticket.exampleImage} alt={ticket.description} />
+        <img
+          width="300"
+          height="300"
+          src={ticket.exampleImage}
+          alt={ticket.description}
+        />
         <NftCollName>Foxcon2022</NftCollName>
         <InnerCont>
           <NftName>{ticket.name}</NftName>
-          { address && chainId === 4 
-            ? <button onClick={mintTicket}>Mint</button> 
-            : !address
-              ? <div>Not Connected to MetaMask</div> 
-              : chainId && chainId !== 4 
-                ? <div>Not Connected to Rinkeby</div>
-                : null 
-          }
+          {address && chainId === 4 ? (
+            <button disabled={isMinting} onClick={mintTicket}>
+              {isMinting ? "Minting..." : "Mint"}
+            </button>
+          ) : !address ? (
+            <div>Not Connected to MetaMask</div>
+          ) : chainId && chainId !== 4 ? (
+            <div>Not Connected to Rinkeby</div>
+          ) : null}
         </InnerCont>
       </NftCard>
     </>
-  )
-}
+  );
+};
 
-export default TicketDetails
+export default TicketDetails;
--- src/context/ViewProvider.js
+++ src/context/ViewProvider.js
@@ -1,92 +1,119 @@
-import { createContext, useEffect, useCallback } from 'react'
-import { useImmerReducer } from 'use-immer'
-import { ethers } from 'ethers'
+import {
+  createContext,
+  useEffect,
+  useCallback,
+  useState,
+  useContext,
+} from "react";
+import { useImmerReducer } from "use-immer";
+import { ethers } from "ethers";
 
-import { initialState } from './initialState.js'
-import { reducer } from '../reducers'
+import { initialState } from "./initialState.js";
+import { reducer } from "../reducers";
 
-import foxcon2022Abi from '../lib/Foxcon2022.json'
+import foxcon2022Abi from "../lib/Foxcon2022.json";
 
-export const ViewContext = createContext(initialState)
+export const ViewContext = createContext(initialState);
+export const useViewContext = () => {
+  const contextValue = useContext(ViewContext);
+  return contextValue;
+};
 
 //utils
 export const bigNumberify = (amt) => {
-  return ethers.utils.parseEther(amt)
-}
+  return ethers.utils.parseEther(amt);
+};
 export const smolNumberify = (amt, decimals = 18) => {
-  return parseFloat(ethers.utils.formatUnits(amt, decimals))
-}
+  return parseFloat(ethers.utils.formatUnits(amt, decimals));
+};
 //utils
 
 export const ViewProvider = ({ children }) => {
-  const [state, dispatch] = useImmerReducer(reducer, initialState)
-  const foxcon2022Address = process.env.REACT_APP_CONTRACT_ADDRESS
+  const [state, dispatch] = useImmerReducer(reducer, initialState);
+  const [isMinting, setIsMinting] = useState(false);
+  const foxcon2022Address = process.env.REACT_APP_CONTRACT_ADDRESS;
 
-  const setAccount = useCallback(async (accounts) => {
-    if (accounts.length > 0) {
-      try {
-        const connectedAccount = {
-          address: accounts[0],
+  const handleSetIsMinting = useCallback((isMinting) => {
+    setIsMinting(isMinting);
+  }, []);
+
+  const setAccount = useCallback(
+    async (accounts) => {
+      if (accounts.length > 0) {
+        try {
+          const connectedAccount = {
+            address: accounts[0],
+          };
+          dispatch({ type: "SET_ACCOUNT", payload: connectedAccount });
+        } catch (e) {
+          console.error(e);
         }
-        dispatch({ type: 'SET_ACCOUNT', payload: connectedAccount })
-      } catch (e) {
-        console.error(e)
+      } else {
+        dispatch({ type: "SET_ACCOUNT", payload: initialState.user });
       }
-    } else {
-      dispatch({ type: 'SET_ACCOUNT', payload: initialState.user })
-    }
-  }, [dispatch])
+    },
+    [dispatch]
+  );
 
   const connectUser = useCallback(async () => {
     try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
       if (provider) {
-        const signer = await provider.getSigner()
-        const { name, chainId } = await provider.getNetwork()
-        const foxcon2022 = new ethers.Contract(foxcon2022Address, foxcon2022Abi.abi, signer)
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' })
-        setAccount(accounts)
+        const signer = await provider.getSigner();
+        const { name, chainId } = await provider.getNetwork();
+        const foxcon2022 = new ethers.Contract(
+          foxcon2022Address,
+          foxcon2022Abi.abi,
+          signer
+        );
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        setAccount(accounts);
         dispatch({
-          type: 'CONNECTED_PROVIDER',
+          type: "CONNECTED_PROVIDER",
           payload: {
             provider,
             signer,
             chainId,
             name,
-            foxcon2022
-          }
-        })
+            foxcon2022,
+          },
+        });
       }
     } catch (e) {
-      console.log(e)
+      console.log(e);
     }
-  }, [setAccount, dispatch])
+  }, [foxcon2022Address, setAccount, dispatch]);
 
   useEffect(() => {
     if (window.ethereum) {
-      connectUser()
-      window.ethereum.on('accountsChanged', () => {
-        connectUser()
-      })
-      window.ethereum.on('chainChanged', () => {
-        connectUser()
-      })
-      window.ethereum.on('disconnect', () => {
-        dispatch({ type: 'SET_ACCOUNT', payload: initialState.user })
-      })
+      connectUser();
+      window.ethereum.on("accountsChanged", () => {
+        connectUser();
+      });
+      window.ethereum.on("chainChanged", () => {
+        connectUser();
+      });
+      window.ethereum.on("disconnect", () => {
+        dispatch({ type: "SET_ACCOUNT", payload: initialState.user });
+      });
     }
-  }, [connectUser, dispatch])
+  }, [connectUser, dispatch]);
 
-  const { foxcon2022, isConnected, signer, name, chainId, provider, user } = state
+  const { foxcon2022, isConnected, signer, name, chainId, provider, user } =
+    state;
 
   const connect = async () => {
     try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-      setAccount(accounts)
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }
+  };
 
   return (
     <ViewContext.Provider
@@ -100,9 +127,12 @@ export const ViewProvider = ({ children }) => {
         user,
         name,
         chainId,
-        actions: { connect }
-      }}>
+        actions: { connect },
+        isMinting,
+        setIsMinting: handleSetIsMinting,
+      }}
+    >
       {children}
     </ViewContext.Provider>
-  )
-}
+  );
+};
